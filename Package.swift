468367d9d7821c598b257a1a7c88cc097f0f5b9f
Cristian Díaz <cristian@210x7.io>
2026-02-07 18:23:28 +0100

    Publish USDInteropAdvanced XCFrameworks v0.2.13

--- Package.swift
+++ Package.swift
@@ -14,18 +14,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "USDInteropAdvancedEditing",
-            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.12/Artifacts/v0.2.12/USDInteropAdvancedEditing-0.2.12.xcframework.zip",
-            checksum: "17d2cd9e3f2e8f9a601e3ecfb08124d18c8bdc601607a446c057eec3b0044c68"
+            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.13/Artifacts/v0.2.13/USDInteropAdvancedEditing-0.2.13.xcframework.zip",
+            checksum: "d339b209ef46107bb9cd023565f41b072cb3459e92d10a910f773a5c42e8ab1e"
         ),
         .binaryTarget(
             name: "USDInteropAdvancedInspection",
-            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.12/Artifacts/v0.2.12/USDInteropAdvancedInspection-0.2.12.xcframework.zip",
-            checksum: "1d115c8385420a1a069e246bcadcce74daea94f38e643e88bf980ca1d7f83b30"
+            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.13/Artifacts/v0.2.13/USDInteropAdvancedInspection-0.2.13.xcframework.zip",
+            checksum: "253d82d71ecf40efaf3ff6c032c8a980ee84c7faeed6c901e8695503bca7fdca"
         ),
         .binaryTarget(
             name: "USDInteropAdvancedWorkflows",
-            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.12/Artifacts/v0.2.12/USDInteropAdvancedWorkflows-0.2.12.xcframework.zip",
-            checksum: "31667909339ab49490d6e28210feb88c317446711cea10e9e7d5b534439c65ab"
+            url: "https://raw.githubusercontent.com/Reality2713/USDInteropAdvanced-binaries/v0.2.13/Artifacts/v0.2.13/USDInteropAdvancedWorkflows-0.2.13.xcframework.zip",
+            checksum: "6b7bc594b5ad9d92ba76321dfdbebb70f724211967d543d10c7c2a90b0509325"
         ),
     ]
 )
